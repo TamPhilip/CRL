2d715705773d090e656753d19f6bfce2bfc24c33
TamPhilip <philip.tam.code@gmail.com>
2019-06-22 11:37:36 -0400

    Initial Commit

---
+++ CRL/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CRL
+//
+//  Created by Philip Tam on 2019-06-22.
+//  Copyright © 2019 Philip Tam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
